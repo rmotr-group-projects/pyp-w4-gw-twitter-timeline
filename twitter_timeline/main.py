70e46337c7b88d03ab8c182550076427a1f53d7b
David Granas <david.granas@gmail.com>
2016-06-12 00:42:09 +0000

    Another way to try it storing followers and following as lists

--- twitter_timeline/main.py
+++ twitter_timeline/main.py
@@ -21,16 +21,9 @@ def before_request():
     g.db = connect_db(settings.DATABASE_NAME)
 
 '''
-http://twitter-timeline-davidgranas.c9users.io:8080/friendship
-body:
-{
-    "username" : "testuser2"
-}
-header:
-Authorization   $RMOTR$-U1
-
 users collection has:
 _id, username, password, first_name, last_name, birth_date
+and two optional parameters: [followers], [following]
 
 tweets collection has:
 _id, user_id, content, created
@@ -38,10 +31,56 @@ _id, user_id, content, created
 auth collections has:
 user_id, access_token
 
-friendships collection: follower -> followed
-follower_id, follower_username, followed_id, followed_username
 '''
 
+def _update_db(d, field, action, value, db):
+    '''
+    If action is 'append' it will either make a new list for d[field] = [value] or 
+    append value to an existing list
+    If action is 'remove' it will remove value from the list
+    After it will update the db with $set
+    '''
+    if action == 'append' and field not in d:
+        d[field] = [value]
+    else:
+        
+        getattr(d[field], action)(value)
+        
+    result= g.db.users.update_one(
+        {'_id': d['_id']},
+        {
+            "$set": {field: d[field]}
+        })
+    return result # returns UpdateResult object
+    
+@app.route('/reset_friendships') # just for testing purposes
+@auth_only
+def reset_friendships(user_id):
+    g.db.users.update_many(
+        {},
+        {
+            "$set": {
+                'following': [],
+                'followers': []
+            }
+        })
+    return Response("Friendships reset", 200)
+    
+@app.route('/show_friendships') # just for testing purposes
+@auth_only
+def show_friendships(user_id):
+    all_users = g.db.users.find({})
+    for user in all_users:
+        print(user['username'])
+        print('Following: '),
+        for person in user.get('following',[]):
+            print(person[1]),
+        print('\nFollowers: '),
+        for person in user.get('followers',[]):
+            print(person[1]),
+        print('\n' + '-'*10)
+    return Response('Printed to terminal', 200)
+    
 @app.route('/friendship', methods=['POST', 'DELETE'])
 @json_only
 @auth_only
@@ -59,76 +98,66 @@ def friendship(user_id):
     
     friend_id = friend_data['_id']
     user_data = g.db.users.find_one({'_id': user_id})
+    user_username = user_data['username']
     
     if user_id == friend_id:
         return Response('{} cannot follow themself'.format(friend_username), 401)
     
     if request.method == 'POST':
-        # find_one will return None if no match found, but find will return a cursor
-        if g.db.friendships.find({'follower_id':user_id,'followed_id':friend_id}).count() > 0:
-            msg = '{} is already following {}'.format(user_data['username'], friend_username)
+        # check if connection already exists
+        if [friend_id, friend_username] in user_data.get('following', []):
+            msg = '{} is already following {}'.format(user_username, friend_username)
             return Response(msg, 404)
             
-        # add the logged in user to the friendships collection of the POST user
-        new_entry = dict(follower_id = user_id,
-                        follower_name = user_data['username'],
-                        followed_id = friend_id,
-                        followed_name = friend_username
-                    )
-                    
-        g.db.friendships.insert_one(new_entry)
-        msg = "Created\n{} is now following {}".format(user_data['username'], friend_username)
+        result1 = _update_db(user_data, 'following', 'append', [friend_id, friend_username], g.db)
+        result2 = _update_db(friend_data, 'followers', 'append', [user_id, user_username], g.db)
+
+        msg = "Created\n{} is now following {}".format(user_username, friend_username)
         return Response(msg, 201)
         
     elif request.method == 'DELETE':
         # make sure entry exists
-        if g.db.friendships.find({'follower_id':user_id,'followed_id':friend_id}).count() == 0:
-            msg = '{} is not following {} so nothing to delete'.format(user_data['username'], friend_username)
+        if [friend_id, friend_username] not in user_data.get('following', []):
+            msg = '{} is not following {} so nothing to delete'.format(user_username, friend_username)
+            return Response(msg, 404)
+        elif [user_id, user_username] not in friend_data.get('followers', []):
+            msg = '{} is not followed by {} which should not happen!'.format(friend_username, user_username)
             return Response(msg, 404)
         
-        g.db.friendships.delete_many({'follower_id': user_id, 
-            'followed_id': friend_data['_id']
-        })
-        # 204 can't include a message body...
+        result1 = _update_db(user_data, 'following', 'remove', [friend_id, friend_username], g.db)
+        result2 = _update_db(friend_data, 'followers', 'remove', [user_id, user_username], g.db)
+            
         return Response('',204)
 
 @app.route('/followers', methods=['GET'])
 @auth_only
 def followers(user_id):
-    # followed will be from the header used_id
-    followed_cursor = g.db.friendships.find({'followed_id':user_id})
-    # get the followers of the followed
-    followers = [dict(username=entry['follower_name'], 
-                uri='/profile/{}'.format(entry['follower_name']))
-                for entry in followed_cursor]
+    entries = g.db.users.find_one({'_id': user_id}).get('followers',[])
+    followers = [dict(username=entry[1], uri='/profile/{}'.format(entry[1])) for entry in entries]
 
     return Response(json.dumps(followers), 200, content_type=JSON_MIME_TYPE) 
 
 @app.route('/timeline', methods=['GET'])
 @auth_only
 def timeline(user_id):
-    # returns list of all tweets of the followed
+    # returns list of all tweets who you are following
         
-    follower_cursor = g.db.friendships.find({'follower_id':user_id})
-    
-    # get the ids of all the followed
-    followed_ids = [entry['followed_id'] for entry in follower_cursor]
+    people = g.db.users.find_one({'_id':user_id}).get('following',[]) # [(id, name), (id, name)...]
     
     all_tweets = []
-    for followed_id in followed_ids:
-        tweet_cursor = g.db.tweets.find({'user_id': followed_id})
+    for person in people:
+        tweet_cursor = g.db.tweets.find({'user_id': person[0]})
         for tweet in tweet_cursor:
             
             new_tweet = dict(created=python_date_to_json_str(tweet['created']), 
                             id = str(tweet['_id']),
                             text=tweet['content'], 
                             uri='/tweet/{}'.format(str(tweet['_id'])),
-                            user_id=str(followed_id))
+                            user_id=str(person[0]))
             all_tweets.append(new_tweet)
             
-    # need to sort by creation date
-    # newlist = sorted(list_to_be_sorted, key=lambda k: k['name']) 
-    sorted_tweets = sorted(all_tweets, key=lambda k: k['created'], reverse=True)
+    # sort by creation date
+    sorted_tweets = sorted(all_tweets, key=lambda x: x['created'], reverse=True)
     
     return Response(json.dumps(sorted_tweets), 200, content_type=JSON_MIME_TYPE) 
 
