89fb5fbdb0b9fc1cd316a5fef5cbebcb5c619df9
Jessica Ngo <jessica.ngo@live.com>
2016-08-05 15:45:02 +0000

    code looks cleaner now

--- twitter_timeline/main.py
+++ twitter_timeline/main.py
@@ -6,18 +6,9 @@ import datetime
 from twitter_timeline import settings
 from twitter_timeline.utils import *
 
-JSON_MIME_TYPE = "application/json"
-
 app = Flask(__name__)
 JSON_MIME_TYPE = 'application/json'
-#First have to figure out authentication in headers - #auth_token = request.headers.get("Authorization")
-#in database (MongoDB) need to probably have 4 tables:
-#user, authentication(to see who is logged in), tweets(to hold tweets)
-#for friendshp table, have a user, and who they are following
-#the friendship collection should contain the username of account owners, and a list of their follower usernames, and their uris
-#for the timeline view, should perform get request on tweets of each user in the list of following
 
-# database_name.collection_name.insert_one/many(dictionaryobject?)
 def connect_db(db_name):
     mongo = MongoClient(settings.FULL_MONGO_HOST)
     return mongo[db_name]
@@ -29,7 +20,7 @@ def before_request():
 
 
 @app.route('/friendship', methods=['POST', 'DELETE'])
-#@json_only
+@json_only
 @auth_only
 def friendship(user_id):
     #Checking if there's a username in our request
@@ -40,17 +31,15 @@ def friendship(user_id):
         friend_id = friend_object["_id"]
         user_object = g.db.users.find_one({"_id": user_id})
     except:
-        print("ERROR: NO USERNAME IN PAYLOAD. NO USERNAME IN DATABASE")
         abort(400)
     
     #check if user is attempting to follow himself/herself
-    if user_id == friend_id:
-        #print (user_id, friend_id)
-        abort(401)
+    if user_id == friend_object["_id"]:
+       abort(401)
         
     #create a set called following for the user object if it doesn't already exist 
     followING = user_object.setdefault("following", [])
-    followERS = friend_object.setdefault("followers", []) #ripping off code yolo 
+    followERS = friend_object.setdefault("followers", []) 
     
     if request.method == 'POST':
         """updates following list for user, and followers list for the followed friend"""
@@ -83,44 +72,30 @@ def followers(user_id):
     followers_return = []
     for _id in follower_id_list:
         follower_object = g.db.users.find_one({"_id": _id})
-        
-        temp_dict = {}
-        temp_dict['username'] = follower_object["username"]
-        temp_dict['uri'] = "/profile/{username}".format(username = temp_dict['username'])
-        followers_return.append(temp_dict)
-        #add to list "followers_return" a dictionary containing the _id's username and uri
-    response = Response(response = json.dumps(followers_return), status = 201, mimetype = JSON_MIME_TYPE)
-    
-    return response
+        followers_return.append({'username': follower_object["username"],
+                                 'uri': "/profile/{}".format(follower_object["username"])
+                                })        
+    return Response(response = json.dumps(followers_return), status = 201, mimetype = JSON_MIME_TYPE)
 
-#first ima try to add followers list
 @app.route('/timeline', methods=['GET'])
 @auth_only
 def timeline(user_id):
     following = _get_user_following(user_id)
-    timeline = []
     all_unsorted_tweets = []
     for _id in following:
         tweets = g.db.tweets.find({"user_id": _id})
         for tweet in tweets:
-            temp_dict = {}
-            temp_dict['created'] = tweet['created']
-            temp_dict['id'] = str(tweet['_id'])
-            temp_dict['text'] = tweet['content']
-            temp_dict['uri'] = "/tweet/{tweet_id}".format(tweet_id = tweet['_id'])
-            temp_dict['user_id'] = str(_id)
-            all_unsorted_tweets.append(temp_dict)
-    timeline = sorted(all_unsorted_tweets, key = _date_sorting, reverse = True)
+            all_unsorted_tweets.append({'created': sqlite_date_to_python(str(tweet['created'])),
+                                        'id':      str(tweet['_id']),
+                                        'text':    tweet['content'],
+                                        'uri':     "/tweet/{}".format(tweet['_id']),
+                                        'user_id': str(_id)
+                                        })            
+    timeline = sorted(all_unsorted_tweets, key = lambda x: x["created"], reverse = True)
     for tweet in timeline:
         tweet['created'] = python_date_to_json_str(tweet['created'])
-    response = Response(response = json.dumps(timeline), status = 200, mimetype= JSON_MIME_TYPE)
-    return response
-    
-
-def _date_sorting(tweet_info):
-    date = sqlite_date_to_python(str(tweet_info['created']))
-    return date
-
+    return Response(response = json.dumps(timeline), status = 200, mimetype= JSON_MIME_TYPE)
+            
 
 @app.errorhandler(404)
 def not_found(e):
@@ -133,11 +108,10 @@ def not_found(e):
 
 def _get_user_followers(user_id):
     user_object = g.db.users.find_one({"_id": user_id})
-    user_object.setdefault("followers", [])
-    return user_object["followers"]
+    return user_object.setdefault("followers", [])
    
 def _get_user_following(user_id):
     user_object = g.db.users.find_one({"_id": user_id})
-    user_object.setdefault("following", [])
-    return user_object["following"]
+    return user_object.setdefault("following", [])
+    
   
