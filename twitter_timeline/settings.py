8f86c655c739e75fd50affd9436f127e9510db02
Preston Rodriguez <prestonr83@gmail.com>
2016-09-17 14:35:59 -0400

    Fixed port in settings

--- twitter_timeline/settings.py
+++ twitter_timeline/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 HOST = 'ds033116.mlab.com'
 USERNAME = 'test_user'
 PASSWORD = 'password1'
-PORT = 44679
+PORT = 33116
 DATABASE_NAME = 'twitter_test_db'
 
 FULL_MONGO_HOST = "mongodb://{usr}:{pwd}@{host}:{port}/{db}".format(
