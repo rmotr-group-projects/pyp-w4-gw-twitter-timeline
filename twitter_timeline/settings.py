b3616c79c16d0ea714a630d692e6eab8c01a56b3
Delio <jangeador@gmail.com>
2016-11-12 18:08:58 +0000

    Implements required solution. Tests passing

--- twitter_timeline/settings.py
+++ twitter_timeline/settings.py
@@ -2,11 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-HOST = None
-USERNAME = None
-PASSWORD = None
-PORT = 13014
-DATABASE_NAME = None
+HOST = 'ds151697.mlab.com'
+USERNAME = 'dbuser'
+PASSWORD = 'donthackmebro'
+PORT = 51697
+DATABASE_NAME = 'twitter-timeline'
 
 FULL_MONGO_HOST = "mongodb://{usr}:{pwd}@{host}:{port}/{db}".format(
     usr=USERNAME,
