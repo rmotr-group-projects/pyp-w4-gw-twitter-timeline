1f5b33d771100e356af41a11dfa7620b6dc0d5a5
contrepoint <contrepoint21@gmail.com>
2016-11-13 17:57:32 +0000

    use sebastian's mlab that actually works this time

--- twitter_timeline/settings.py
+++ twitter_timeline/settings.py
@@ -2,11 +2,17 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-HOST = 'ds033116.mlab.com'
-USERNAME = 'seg2151'
-PASSWORD = '1pythoniscool1'
-PORT = '33116'
-DATABASE_NAME = 'rmotr_twitter_timeline'
+HOST = "ds151917.mlab.com"
+USERNAME = "seg2151"
+PASSWORD = "password1"
+PORT = 51917
+DATABASE_NAME = "twitter_timeline"
+
+# HOST = 'ds033116.mlab.com'
+# USERNAME = 'test_user'
+# PASSWORD = 'password1'
+# PORT = 33116
+# DATABASE_NAME = 'twitter_test_db'
 
 FULL_MONGO_HOST = "mongodb://{usr}:{pwd}@{host}:{port}/{db}".format(
     usr=USERNAME,
