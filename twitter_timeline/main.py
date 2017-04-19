ae71a516e198cf5a11b9ef0e7317ecf668b973f8
Rudo <rudoterry@gmail.com>
2017-04-19 02:33:31 +0000

    tests pass

--- twitter_timeline/main.py
+++ twitter_timeline/main.py
@@ -1,8 +1,8 @@
 import json
 
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 from bson.objectid import ObjectId
-from flask import Flask, g
+from flask import Flask, g, jsonify
 
 from twitter_timeline import settings
 from twitter_timeline.utils import *
@@ -26,20 +26,59 @@ def before_request():
 @json_only
 @auth_only
 def friendship(user_id):
-    pass
-
+    json_data = request.get_json()
+    username = json_data.get('username', None)
+    if not username:
+        abort(400)
+    actual_user = g.db.users.find_one({'_id': ObjectId(user_id)})
+    want_user = g.db.users.find_one({'username': username})
+    if not want_user:
+        abort(400)
+    want_user_id = want_user['_id']
+    
+    if request.method == 'POST':
+        g.db.friendships.insert({'user_id':user_id, 'username': username, 'friend_id':want_user_id,})
+        return '',201
+    if request.method == 'DELETE':
+        g.db.friendships.delete_one({'user_id':user_id,'friend_id':want_user_id})
+        return '',204
+        
+    
+def get_username(user_id):
+    return g.db.users.find_one({'_id': ObjectId(user_id)})['username']
 
 @app.route('/followers', methods=['GET'])
 @auth_only
 def followers(user_id):
-    pass
+    followers = g.db.friendships.find({'friend_id': ObjectId(user_id)})
+    
+    follower_list =[]
+    for f in followers:
+        username = get_username(f['user_id'])
+        follower_list.append({'username': username, 'uri': '/profile/{}'.format(username)})
+    
+    return jsonify(follower_list)
 
 
 @app.route('/timeline', methods=['GET'])
 @auth_only
 def timeline(user_id):
-    pass
-
+    user_friends = g.db.friendships.find({'user_id':user_id})
+    print(user_friends.count())
+    user_id_followed = [ObjectId(f['friend_id']) for f in user_friends]
+    
+    if len(user_id_followed) ==0:
+        return jsonify([]),200
+    tweet_LIST = []
+    tweets = g.db.tweets.find({'user_id':{'$in': user_id_followed}}).sort('created',DESCENDING)
+    for tweet in tweets:
+        tweet_LIST.append({'created': python_date_to_json_str(tweet['created']),
+                            'id': str(tweet['_id']),
+                            'text': tweet['content'],
+                            'uri': '/tweet/{}'.format(tweet['_id']),
+                            'user_id': str(tweet['user_id'])
+        })
+    return jsonify(tweet_LIST)
 
 @app.errorhandler(404)
 def not_found(e):
--- twitter_timeline/settings.py
+++ twitter_timeline/settings.py
@@ -1,12 +1,13 @@
 import os
+#mongodb://<dbuser>:<dbpassword>@ds163340.mlab.com:63340/timeline
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-HOST = None
-USERNAME = None
-PASSWORD = None
-PORT = 13014
-DATABASE_NAME = None
+HOST = 'ds163340.mlab.com'
+USERNAME = 'c9_users'
+PASSWORD = 'facebook1'
+PORT = 63340
+DATABASE_NAME = 'timeline'
 
 FULL_MONGO_HOST = "mongodb://{usr}:{pwd}@{host}:{port}/{db}".format(
     usr=USERNAME,
