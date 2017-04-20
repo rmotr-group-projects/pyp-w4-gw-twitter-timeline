14f78fa408d3ace467ef57bea4771105e2aab5c2
Jason Symons <jason.symons@gmail.com>
2017-04-20 15:23:49 -0400

    Testing

--- twitter_timeline/settings.py
+++ twitter_timeline/settings.py
@@ -2,11 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-HOST = None
-USERNAME = None
-PASSWORD = None
-PORT = 13014
-DATABASE_NAME = None
+HOST = 'ds131340.mlab.com'
+USERNAME = 'testuser'
+PASSWORD = 'donthackmebro'
+PORT = 31340
+DATABASE_NAME = 'testytest123'
 
 FULL_MONGO_HOST = "mongodb://{usr}:{pwd}@{host}:{port}/{db}".format(
     usr=USERNAME,
