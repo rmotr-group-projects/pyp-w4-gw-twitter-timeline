28238a90e37c38bb1ecf1bb2be300f2ae10f33e0
Marianna <mangat.marianna@gmail.com>
2017-06-28 03:21:28 +0000

    Initial commit

--- twitter_timeline/main.py
+++ twitter_timeline/main.py
@@ -1,8 +1,9 @@
 import json
 
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 from bson.objectid import ObjectId
-from flask import Flask, g
+from flask import Flask, g, request, jsonify, make_response, abort
+import datetime
 
 from twitter_timeline import settings
 from twitter_timeline.utils import *
@@ -21,24 +22,93 @@ def connect_db(db_name):
 def before_request():
     g.db = connect_db(settings.DATABASE_NAME)
 
-
 @app.route('/friendship', methods=['POST', 'DELETE'])
 @json_only
 @auth_only
-def friendship(user_id):
-    pass
-
-
+def friendship(user_id):            
+    # assume friendship table looks like this:
+    # {'user_id': <user_1>, 'following_ids': [<user_2>, <user_3>,...]}
+    # {'user_id': <base_user>, 'follows': <target_user>}
+    # {'user_id': <base_user>, 'follows': <target_user2>}
+    # {'user_id': <base_user2>, 'follows': <target_user>}
+    
+    
+    json_dict = request.get_json() 
+    
+    try:
+        username = json_dict.get('username',None)
+    except KeyError:
+        return abort(400)
+    
+    ##if not username:
+    ##    return abort(400)
+        
+    
+    following_user = g.db.users.find_one({'username': username})
+    if not following_user:
+        return abort(400)
+        
+    following_userid = ObjectId(following_user['_id'])
+    existing_follow = g.db.friendships.find_one({'user_id':user_id, 'following_id':following_userid})
+    
+    if request.method == 'POST':
+        
+        g.db.friendships.insert_one({'user_id': user_id, 'following_id': following_userid}) 
+        return '', 201 
+    
+    elif request.method == 'DELETE':
+        if existing_follow:
+            g.db.friendships.delete_one({'user_id':user_id, 'following_id':following_userid})
+            return '', 204
+        else:
+            return abort(401)
+        
 @app.route('/followers', methods=['GET'])
 @auth_only
 def followers(user_id):
-    pass
-
+    
+    base_user = g.db.friendships.find({'following_id': ObjectId(user_id)})
+    results_list = []
+    for user in base_user:
+        
+        follower = g.db.users.find_one({'_id': ObjectId(user['user_id'])})
+        follower_username = follower['username']
+        
+        results_list.append({'username': follower_username, 'uri': '/profile/{}'.format(follower_username)})
+    
+ 
+    return jsonify(results_list), 201
+ 
+    
 
 @app.route('/timeline', methods=['GET'])
 @auth_only
 def timeline(user_id):
-    pass
+    followers = g.db.friendships.find({ "user_id": user_id })
+    
+    following_ids_list = [ObjectId(f['following_id']) for f in followers]
+    
+    if len(following_ids_list) == 0:
+        return jsonify([]), 200 
+ 
+    tweets = g.db.tweets.find({'user_id': {"$in": following_ids_list}}).sort("created", DESCENDING)
+    
+    tweets_list = []
+    for twt in tweets:
+        
+#        print '{} {}'.format(twt['content'], twt['_id'])
+        
+        tweets_list.append(
+            {'created': datetime.strftime(twt['created'],'%Y-%m-%dT%H:%M:%S'),
+             'id': str(twt['_id']),
+             'text': twt['content'],
+             'uri': '/tweet/{}'.format(str(twt['_id'])),
+             'user_id': str(twt['user_id'])
+        })
+        
+        
+    #return jsonify(tweets_list), 200
+    return json.dumps(tweets_list).encode('utf8'), 200
 
 
 @app.errorhandler(404)
@@ -47,5 +117,6 @@ def not_found(e):
 
 
 @app.errorhandler(401)
-def not_found(e):
+def not_found1(e):
     return '', 401
+
--- twitter_timeline/settings.py
+++ twitter_timeline/settings.py
@@ -2,15 +2,19 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-HOST = None
-USERNAME = None
-PASSWORD = None
-PORT = 13014
-DATABASE_NAME = None
+# mongodb://twitter-timeline1:timeline123@ds139242.mlab.com:39242/twitter-timeline
+
+# % mongo ds139242.mlab.com:39242/twitter-timeline -u twitter-timeline1 -p timeline123
+
+HOST = "ds139242.mlab.com"
+USERNAME = "twitter-timeline1"   
+PASSWORD = "timeline123"
+PORT = 39242
+DATABASE_NAME = "twitter-timeline"
 
 FULL_MONGO_HOST = "mongodb://{usr}:{pwd}@{host}:{port}/{db}".format(
     usr=USERNAME,
-    pwd=PASSWORD,
+    pwd=PASSWORD, 
     host=HOST,
     port=PORT,
     db=DATABASE_NAME)
